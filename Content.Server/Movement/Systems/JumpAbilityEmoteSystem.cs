b06d52fefa37a5c2351c678b4a740e7578ba66ad
Sprinkle <some@test.com>
2026-06-03 23:05:02 +0300

    small fixes

--- Content.Server/Movement/Systems/JumpAbilityEmoteSystem.cs
+++ Content.Server/Movement/Systems/JumpAbilityEmoteSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2026 Sprinkle <40203084+lnn0q@users.noreply.github.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.Chat.Systems;
 using Content.Shared.Movement.Components;
 using Content.Shared.Movement.Systems;
