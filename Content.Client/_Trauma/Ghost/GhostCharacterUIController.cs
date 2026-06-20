Kenkaitos/Trauma-Station

reinforcements and ninja let you pick character to use (#1556)
Co-authored-by: deltanedas <@deltanedas:kde.org>

Content.Trauma.Client/Ghost/GhostCharacterUIController.cs
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Client.UserInterface.Systems.Ghost;
+using Content.Client.UserInterface.Systems.Ghost.Widgets;
+using Content.Trauma.Client.Ghost.UI;
+using Content.Trauma.Shared.Ghost;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controllers;
+using Robust.Client.UserInterface.Controls;
+
+namespace Content.Trauma.Client.Ghost;
+
+public sealed class GhostCharacterUIController : UIController
+{
+    [UISystemDependency] private readonly GhostCharacterSystem _character = default!;
+
+    public const string ButtonName = "PickCharacterButton";
+
+    private GhostCharacterWindow? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        GhostUIController.OnGuiLoaded += OnGuiLoaded;
+    }
+
+    private void OnGuiLoaded(GhostGui gui)
+    {
+        var container = gui.GetChild(0);
+        // don't add duplicate buttons
+        foreach (var child in container.Children)
+        {
+            if (child.Name == ButtonName)
+                return; // already added
+        }
+
+        var button = new Button()
+        {
+            Name = ButtonName,
+            Text = Loc.GetString("ghost-gui-pick-character-button")
+        };
+        button.OnPressed += _ => TogglePickCharacter();
+        container.AddChild(button);
+    }
+
+    private void TogglePickCharacter()
+    {
+        if (_window is {})
+        {
+            _window.Close();
+            return;
+        }
+
+        _window = new(_character.GetLocalData() ?? new());
+        _window.OpenCentered();
+        _window.OnSlotSet += slot => _character.SetGhostRoleSlot(slot);
+        _window.OnClose += () =>
+        {
+            _window = null;
+        };
+    }
+}

Content.Trauma.Client/Ghost/UI/GhostCharacterWindow.xaml.cs
@@ -1,0 +1,57 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Client.Lobby;
+using Content.Client.UserInterface.Controls;
+using Content.Trauma.Shared.Ghost;
+using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+
+namespace Content.Trauma.Client.Ghost.UI;
+
+[GenerateTypedNameReferences]
+public sealed partial class GhostCharacterWindow : FancyWindow
+{
+    [Dependency] private readonly IClientPreferencesManager _prefs = default!;
+
+    public event Action<int?>? OnSlotSet;
+    public RadioOptions<int?> Buttons = new(RadioOptionsLayout.Vertical);
+
+    public GhostCharacterWindow(CharacterData data)
+    {
+        RobustXamlLoader.Load(this);
+        IoCManager.InjectDependencies(this);
+
+        ScrollContainer.AddChild(Buttons);
+
+        AddCharacterButtons(data);
+        AddRandomOption();
+
+        Buttons.SelectByValue(data.DesiredSlot);
+
+        Buttons.OnItemSelected += args =>
+        {
+            Buttons.Select(args.Id);
+            OnSlotSet?.Invoke(Buttons.SelectedValue);
+        };
+    }
+
+    private void AddCharacterButtons(CharacterData data)
+    {
+        if (_prefs.Preferences is not {} prefs)
+            return;
+
+        foreach (var (slot, profile) in prefs.Characters)
+        {
+            var name = profile.Name;
+            var idx = Buttons.AddItem(name, slot);
+            // don't allow picking already spawned characters
+            Buttons.SetItemDisabled(idx, data.SpawnedNames.Contains(name));
+        }
+    }
+
+    private void AddRandomOption()
+    {
+        Buttons.AddItem(Loc.GetString("ghost-character-window-random-character"), null);
+    }
+}

CahntapDypkn/Rat_Bite_Station_14

Content.Client/_Trauma/Ghost/GhostCharacterUIController.cs
@@ -1,0 +1,63 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Client.UserInterface.Systems.Ghost;
+using Content.Client.UserInterface.Systems.Ghost.Widgets;
+using Content.Client._Trauma.Ghost.UI;
+using Content.Trauma.Shared.Ghost;
+using Robust.Client.UserInterface;
+using Robust.Client.UserInterface.Controllers;
+using Robust.Client.UserInterface.Controls;
+
+namespace Content.Client._Trauma.Ghost;
+
+public sealed class GhostCharacterUIController : UIController
+{
+    [UISystemDependency] private readonly GhostCharacterSystem _character = default!;
+
+    public const string ButtonName = "PickCharacterButton";
+
+    private GhostCharacterWindow? _window;
+
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        GhostUIController.OnGuiLoaded += OnGuiLoaded;
+    }
+
+    private void OnGuiLoaded(GhostGui gui)
+    {
+        var container = gui.GetChild(0);
+        // don't add duplicate buttons
+        foreach (var child in container.Children)
+        {
+            if (child.Name == ButtonName)
+                return; // already added
+        }
+
+        var button = new Button()
+        {
+            Name = ButtonName,
+            Text = Loc.GetString("ghost-gui-pick-character-button")
+        };
+        button.OnPressed += _ => TogglePickCharacter();
+        container.AddChild(button);
+    }
+
+    private void TogglePickCharacter()
+    {
+        if (_window is {})
+        {
+            _window.Close();
+            return;
+        }
+
+        _window = new(_character.GetLocalData() ?? new());
+        _window.OpenCentered();
+        _window.OnSlotSet += slot => _character.SetGhostRoleSlot(slot);
+        _window.OnClose += () =>
+        {
+            _window = null;
+        };
+    }
+}

Content.Client/_Trauma/Ghost/UI/GhostCharacterWindow.xaml.cs
@@ -1,0 +1,57 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Client.Lobby;
+using Content.Client.UserInterface.Controls;
+using Content.Trauma.Shared.Ghost;
+using Robust.Client.AutoGenerated;
+using Robust.Client.UserInterface.Controls;
+using Robust.Client.UserInterface.XAML;
+
+namespace Content.Client._Trauma.Ghost.UI;
+
+[GenerateTypedNameReferences]
+public sealed partial class GhostCharacterWindow : FancyWindow
+{
+    [Dependency] private readonly IClientPreferencesManager _prefs = default!;
+
+    public event Action<int?>? OnSlotSet;
+    public RadioOptions<int?> Buttons = new(RadioOptionsLayout.Vertical);
+
+    public GhostCharacterWindow(CharacterData data)
+    {
+        RobustXamlLoader.Load(this);
+        IoCManager.InjectDependencies(this);
+
+        ScrollContainer.AddChild(Buttons);
+
+        AddCharacterButtons(data);
+        AddRandomOption();
+
+        Buttons.SelectByValue(data.DesiredSlot);
+
+        Buttons.OnItemSelected += args =>
+        {
+            Buttons.Select(args.Id);
+            OnSlotSet?.Invoke(Buttons.SelectedValue);
+        };
+    }
+
+    private void AddCharacterButtons(CharacterData data)
+    {
+        if (_prefs.Preferences is not {} prefs)
+            return;
+
+        foreach (var (slot, profile) in prefs.Characters)
+        {
+            var name = profile.Name;
+            var idx = Buttons.AddItem(name, slot);
+            // don't allow picking already spawned characters
+            Buttons.SetItemDisabled(idx, data.SpawnedNames.Contains(name));
+        }
+    }
+
+    private void AddRandomOption()
+    {
+        Buttons.AddItem(Loc.GetString("ghost-character-window-random-character"), null);
+    }
+}